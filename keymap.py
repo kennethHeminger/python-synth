f72dc808edb169abba5b9fcadbf7e1a0f2d81d08
kenny <khemi11@wgu.edu>
2026-02-11 21:38:48 +0100

    Stage 3.4:
    - Fixed volume drops when multiple notes are played simultaneously by using square root scaling
    
    To do:
    - implement additional oscillators
    - implement reverb
    - implement delay
    - design a virtual keyboard in pygame to act as a MIDI for oscillators/future features and show keys

--- keymap.py
+++ keymap.py
@@ -9,7 +9,7 @@ key_to_freq = {
     pygame.K_g: 329.63,         # E4
     pygame.K_h: 349.23,         # F4
     pygame.K_j: 369.99,         # F#4 / Gb4
-    pygame.K_k: 392.00,         # G4kl
+    pygame.K_k: 392.00,         # G4
     pygame.K_l: 415.30,         # G#4 / Ab4
     pygame.K_SEMICOLON: 440.00, # A4
     pygame.K_QUOTE: 466.16,     # A#4 / Bb4
